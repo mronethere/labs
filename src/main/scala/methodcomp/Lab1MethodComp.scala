a94658720fe7d81cb5f0b96900f17202b002a4ff
mentegy <mentegy@gmail.com>
2015-09-14 18:19:42 +0300

    add InputData/OutputData case classes

--- src/main/scala/methodcomp/Lab1MethodComp.scala
+++ src/main/scala/methodcomp/Lab1MethodComp.scala
@@ -1,5 +1,12 @@
 package methodcomp
 
-object Lab1MethodComp {
+import scala.concurrent.Future
 
+object Lab1MethodComp {
+  def solve(data: InputData) = Future {
+
+  }
+
+  case class InputData(e: Double, swap: Boolean, fun: String, funD: String, a: Double, b: Double)
+  case class OutputData(halfDivision: Double, chords: Double, newton: Double, iterating: Double)
 }
