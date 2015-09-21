ac311fb5745a5ee13f101e4d71b46c005871f9f7
mentegy <mentegy@gmail.com>
2015-09-21 18:05:36 +0300

    UI - implement dropdown menu for labs (not available in nested view, because they are not literally nested)

--- src/main/resources/app/main/app.js
+++ src/main/resources/app/main/app.js
@@ -3,14 +3,27 @@
     'use strict';
 
     define([
-            './ContainerController',
             '../methodcomp/MethodCompModule',
             '../graphics/GraphicsModule'
-        ], function (ContainerController, MethodCompModule, GraphicsModule) {
+        ], function (MethodCompModule, GraphicsModule) {
             var appName = 'labs';
             angular
                 .module(appName, ['ui.router', 'ui.bootstrap', MethodCompModule, GraphicsModule])
-                .controller('ContainerController', ContainerController)
+                .controller('MenuController', ['$state', function ($state) {
+                    this.state = $state;
+                    console.log($state);
+                    var menu = {
+                        methodcomp: 2,
+                        graphics: 0
+                    };
+                    for (var prop in menu) {
+                        this[prop] = [];
+                        for (var i = 1; i <= menu[prop]; i++) {
+                            this[prop].push('#/' + prop + '/' + i);
+                        }
+                    }
+                    console.dir(this);
+                }])
                 .config(function ($stateProvider, $urlRouterProvider) {
                     $urlRouterProvider.otherwise('/home');
 
