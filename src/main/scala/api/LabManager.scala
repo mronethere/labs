a045cf2af863f3e367964ad87ca144ed3337d0a4
mentegy <mentegy@gmail.com>
2015-09-23 14:05:29 +0300

    fix timeout handling for LabManager.solveLab method

--- src/main/scala/api/LabManager.scala
+++ src/main/scala/api/LabManager.scala
@@ -2,7 +2,7 @@ package api
 
 import methodcomp._
 
-import scala.concurrent.Future
+import scala.concurrent.{TimeoutException, Future}
 import scala.concurrent.duration._
 import core.Boot.system
 import core.Boot.system._
@@ -10,12 +10,14 @@ import akka.pattern.after
 
 object LabManager {
 
-  def solveLab(data: LabData): Future[LabData] = after(5 second, system.scheduler) {
-    labs(data.projectName, data.labId.toString).solve(data)
-  } map {
-    case list => LabData(data.projectName, data.labId, list, isSuccess = true)
-  } recover {
-    case e => LabData(data.projectName, data.labId, List(e.getMessage), isSuccess = false)
+  def solveLab(data: LabData): Future[LabData] = {
+    val timeout = after(5.second, using = system.scheduler)(Future.failed(new Error("Timeout exception")))
+    val result = labs(data.projectName, data.labId.toString).solve(data).map {
+      case list => LabData(data.projectName, data.labId, list, isSuccess = true)
+    }
+    Future firstCompletedOf Seq(timeout, result) recover {
+      case e => LabData(data.projectName, data.labId, List(e.getMessage), isSuccess = false)
+    }
   }
 
 
