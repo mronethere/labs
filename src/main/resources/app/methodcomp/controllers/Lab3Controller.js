41a4849e2297eb7b0ee427df00a6e080651dad97
mentegy <mentegy@gmail.com>
2015-09-28 12:13:16 +0300

    Lab3MethodComp - finish UI part

--- src/main/resources/app/methodcomp/controllers/Lab3Controller.js
+++ src/main/resources/app/methodcomp/controllers/Lab3Controller.js
@@ -6,26 +6,53 @@
     define([],
         function () {
             var Lab3Controller = function ($labs) {
-                var controller = this;
-                controller.matrix = [[]];
+                var lab = this;
+                lab.matrix = [[]];
 
                 this.submit = function() {
-                    $labs.process('methodcomp', 3, [
-                        controller.E, controller.swap,
-                        controller.func, controller.A, controller.B
-                    ]).then(function (data) {
-                        if (data['isSuccess']) {
-                            controller.rez = data['params'][0];
+                    var arr = [];
+                    arr.push(lab.size);
+                    for (var i in lab.matrix) {
+                        for (var j in lab.matrix[i]) {
+                            arr.push(lab.matrix[i][j]);
                         }
-                    });
+                    }
+                    $labs
+                        .process('methodcomp', 3, arr)
+                        .then(function (data) {
+                            if (data['isSuccess']) {
+                                var arr = data['params'];
+                                var i = 0;
+                                if (arr[i] = 'gaussian') {
+                                    var rez = "";
+                                    while (arr[++i] != 'cramer' && i < 100) {
+                                        rez += arr[i] + ", ";
+                                    }
+                                    lab.result1 = rez;
+                                    rez = "";
+                                    while (arr[++i] != 'inverse' && i < 100) {
+                                        rez += arr[i] + ", ";
+                                    }
+                                    lab.result2 = rez;
+                                    rez = "";
+                                    while (++i < arr.length) {
+                                        rez += arr[i] + ", ";
+                                    }
+                                    lab.result3 = rez;
+                                }
+
+
+                            }
+                            console.dir(data);
+                        });
                 };
                 
                 this.changeSize = function (size) {
-                    controller.matrix =[];
+                    lab.matrix =[];
                     for (var i = 0; i < size; i++) {
-                        controller.matrix.push([]);
+                        lab.matrix.push([]);
                         for (var j = 0; j < size + 1; j++) {
-                            controller.matrix[i].push(0);
+                            lab.matrix[i].push(0);
                         }
                     }
                 }
